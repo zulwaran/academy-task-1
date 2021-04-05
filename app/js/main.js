757552696491d549491f6ad995489a9876bf8044
zulwaran <81923601+zulwaran@users.noreply.github.com>
2021-04-06 05:40:26 +0700

    Третий коммит
    Сверстал раздел Testimonials. Подключил слик-слайдер, переделал цвет фона в разделах Features и How Does It Works?

---
+++ app/js/main.js
@@ -0,0 +1,15 @@
+$(function () {
+    $('.testimonials__slider').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrows: false,
+        infinite:true,
+        dots:true,
+        variableWidth:true,
+    });
+
+
+
+
+
+})
