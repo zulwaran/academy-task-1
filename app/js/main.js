01016e61f2929be938355fa04d5b9a2d683b629a
zulwaran <81923601+zulwaran@users.noreply.github.com>
2021-04-09 08:59:56 +0700

    Шестой коммит
    Внес правки в верстку, сделал адаптив

--- app/js/main.js
+++ app/js/main.js
@@ -6,9 +6,28 @@ $(function () {
         infinite:true,
         dots:true,
         variableWidth:true,
-    });
+      });
+
+    $('.btn-year').on('click', function(){
+      $('.cost-month').addClass('off');
+      $('.cost-month').removeClass('on');
+      $('.cost-year').addClass('on');
+      $('.cost-year').removeClass('off');
+      $('.btn-month').removeClass('active');
+      $('.btn-year').addClass('active');
+      });
+    $('.btn-month').on('click', function(){
+      $('.cost-month').addClass('on');
+      $('.cost-month').removeClass('off');
+      $('.cost-year').addClass('off');
+      $('.cost-year').removeClass('on');
+      $('.btn-year').removeClass('active');
+      $('.btn-month').addClass('active');
+      });
+
+
     function scrollNav() {
-        $('.nav a, .scrollDown, .scrollUp').click(function(){
+        $('.nav a, .scrollDown, .scrollUp').on('click', function(){
           $('html, body').stop().animate({
             scrollTop: $($(this).attr('href')).offset().top - 160
           }, 300);
